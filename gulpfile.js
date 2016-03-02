74d7c91823bf71065543ec24a594658e85ec8509
Dmitry Koval <koval@outlook.com>
2016-03-03 01:31:17 +0200

    Set up browser spinning and auto-refresh when src/index.html changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,29 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var runSequence = require('run-sequence');
+
+// Development Tasks 
+// -----------------
+
+// Start browserSync server
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'src'
+    }
+  })
+})
+
+// Watchers
+gulp.task('watch', function() {
+  gulp.watch('src/*.html', browserSync.reload);
+})
+
+// Build Sequences
+// ---------------
+
+gulp.task('default', function(callback) {
+  runSequence(['browserSync', 'watch'],
+    callback
+  )
+})
